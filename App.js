c4ca4f11d9a0af90052e390b7383287a359106ed
Victor <victor.parodi.montero@gmail.com>
2017-09-13 13:01:17 -0500

    rama3 commit 1: componentes de header y lista de albunes listos

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
+import { View } from 'react-native';
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
-  <Header />
+  <View>
+    <Header headerText={'Albumns: R3!'} />
+    <AlbumList />
+  </View>
 );
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
-const Header = () => {
-    const {textStyle} = style;
+const Header = (props) => {
+    const {textStyle, viewStyle} = style;
 
-    return <Text style={textStyle}> Albums </Text>
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}> {props.headerText} </Text>
+        </View>
+    )
 }
 
 const style = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        alignItems: 'center',
+        height: 60,
+        justifyContent: 'center',
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+    },
+
     textStyle: {
         fontSize: 20
     }
