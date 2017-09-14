3d97ca02b42b9335245f2faa33bb2b3f85b291a9
Victor <victor.parodi.montero@gmail.com>
2017-09-14 12:34:36 -0500

    master commit 2: agregando axios al proyecto. Haciendo un llamado AJAX y haciend controles con el estado del cmponente

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
-    return (
-        <View>
-            <Text> Album List!!! </Text>
-        </View>
-    )
+class AlbumList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            albums: []
+        }
+    }
+
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data }));
+    }
+
+    render() {
+        console.log(this.state.albums);
+
+        return (
+            <View>
+                <Text> Album List!!! </Text>
+            </View>
+        )
+    }
 }
 
 export default AlbumList;
