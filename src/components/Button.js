cbbe6c1e76dbe44df04a745d4a7569fd65e211a9
Victor <victor.parodi.montero@gmail.com>
2017-09-18 10:32:11 -0500

    rama 3 commit 4. agregando estilos al botón

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,35 @@ import React, { Component } from 'react';
 import { Text, TouchableOpacity } from 'react-native'; 
 
 const Button = () => {
+    const { textStyle, buttonStyle } = style;
+
     return (
-        <TouchableOpacity>
-            <Text> Click me!!! </Text>
+        <TouchableOpacity style={buttonStyle}>
+            <Text style={textStyle}> Click me!!! </Text>
         </TouchableOpacity>
     )
+}
+
+const style = {
+    textStyle: {
+        alignSelf: 'center',
+        color: '#007AFF',
+        fontSize: 16,
+        fontWeight: '600',
+        paddingBottom: 10,
+        paddingTop: 10
+    },
+
+    buttonStyle: {
+        flex: 1,
+        alignSelf: 'stretch',
+        backgroundColor: '#FFF',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#007AFF',
+        marginLeft: 5,
+        marginRight: 5
+    }
 }
 
 export default Button;
